deb2e23ff1aaa91e1ce13ac9da7aa475cc5989fe
Daniel Liao <liaodzn@gmail.com>
2024-09-15 16:01:52 -0400

    refactor(portfolio): update information in site sections to reflect current skills and experience

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -10,7 +10,7 @@ const Nav = () => {
   const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-      <a href="#" onClick = {() => setActiveNav('#')} className = {activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
+      <a href="#header" onClick = {() => setActiveNav('#header')} className = {activeNav === '#header' ? 'active' : ''}><AiOutlineHome /></a>
       <a href="#about" onClick = {() => setActiveNav('#about')} className = {activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
       <a href="#experience" onClick = {() => setActiveNav('#experience')} className = {activeNav === '#experience' ? 'active' : ''}><BiBook /></a>
       <a href="#portfolio" onClick = {() => setActiveNav('#portfolio')} className = {activeNav === '#portfolio' ? 'active' : ''}><RiServiceLine /></a>
