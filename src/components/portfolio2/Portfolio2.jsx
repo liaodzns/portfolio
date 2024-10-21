4e3d4f1b3161eddfcabe093d2942293f03e7e430
Daniel Liao <din0liao837@gmail.com>
2024-10-21 18:36:19 -0400

    fix: pagination

--- src/components/portfolio2/Portfolio2.jsx
+++ src/components/portfolio2/Portfolio2.jsx
@@ -13,20 +13,6 @@ import 'swiper/css/pagination';
 
 const data = [
     {
-        name: 'Stock Prediction Model',
-        description: [
-            '- Created a geodemographic segmentation model based on 11 conditions to determine customers of a bank with a high risk of leaving',
-            '- Used basic knowledge of artificial neural networks to train algorithm to make correct predictions'
-        ]
-    },
-    {
-        name: 'Financial ANN model',
-        description: [
-            '- Created a geodemographic segmentation model based on 11 conditions to determine customers of a bank with a high risk of leaving',
-            '- Used basic knowledge of artificial neural networks to train algorithm to make correct predictions'
-        ]
-    },
-    {
         name: 'Chinese Vocabulary Learning Application',
         description: [
             '- Created a full-stack web application to help users learn Chinese vocabulary',
@@ -35,11 +21,25 @@ const data = [
         ]
     },
     {
+        name: 'Stock Prediction Model',
+        description: [
+            '- Developed a LSTM Neural Network using TensorFlow and Keras to predict the closing prices of stocks, leveraging historical stock data from yfinance API and incorporating technical indicators such as Moving Averages, MACD, and Bollinger Bands for feature enhancement',
+            '- Developed and engineered time-series data for model training and created interactive visualizations using matplotlib to compare real vs predicted prices across multiple stocks'
+        ]
+    },
+    {
         name: 'Spotify Clone',
         description: [
             '- Created a music streaming application using React and Tailwind, replicating the popular features and functionalities of Spotify',
             '- Integrated Stripe to enable premium subscriptions',
             '- Created database schemas and implemented authentication using Supabase, ensuring secure user registration and login processes'
+        ]
+    },
+    {
+        name: 'Financial ANN model',
+        description: [
+            '- Created a geodemographic segmentation model based on 11 conditions to determine customers of a bank with a high risk of leaving',
+            '- Used basic knowledge of artificial neural networks to train algorithm to make correct predictions'
         ]
     },
     {
